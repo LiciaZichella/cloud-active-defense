af9f6588aef91fac635a748d3a45eba1df4e3c01
Licia <licia.zichella@gmail.com>
2026-03-04 18:29:06 +0100

    Aggiunto script IAM per Least Privilege e file gitignore

---
+++ infra/init-scripts/02-create-iam.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Inizio configurazione identita' IAM (Least Privilege)..."
+
+# 1. Creazione del ruolo 'Employee' (Il dipendente standard)
+awslocal iam create-role \
+    --role-name EmployeeRole \
+    --assume-role-policy-document '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"Service":"ec2.amazonaws.com"},"Action":"sts:AssumeRole"}]}'
+echo "Ruolo 'EmployeeRole' creato con successo."
+
+# 2. Creazione del ruolo 'SecurityAdmin' (L'amministratore che riceve gli allarmi)
+awslocal iam create-role \
+    --role-name SecurityAdminRole \
+    --assume-role-policy-document '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"Service":"ec2.amazonaws.com"},"Action":"sts:AssumeRole"}]}'
+echo "Ruolo 'SecurityAdminRole' creato con successo."
+
+echo "Configurazione IAM completata!"
